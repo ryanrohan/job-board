17099a885d64e154e782a3b56ff3cee6966717f1
Ryan Rohan <ryanrohan@Ryans-MacBook-Pro.local>
2026-05-20 02:28:35 -0400

    use env var for API URL

--- frontend/lib/apollo.ts
+++ frontend/lib/apollo.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000',
+  uri: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000',
 });
 
 const authLink = setContext((_, { headers }) => {
